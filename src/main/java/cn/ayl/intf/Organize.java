f7a6329f3ad7fac3203dfadb77d897e16988d17a
anyongliang <AnYongLiang4869@163.com>
2019-11-13 21:24:17 +0800

    修改：一些注解

--- src/main/java/cn/ayl/intf/Organize.java
+++ src/main/java/cn/ayl/intf/Organize.java
@@ -9,14 +9,14 @@ import cn.ayl.util.json.JsonObject;
  * created by Rock-Ayl 2019-11-13
  * 用来测试
  */
-@Service(desc = "测试相关")
+@Service(desc = "测试相关，所有继承于IMicroService的都是服务")
 public interface Organize extends IMicroService {
 
-    @Method(desc = "方法相关", auth = true)
+    @Method(desc = "方法相关，auth逻辑暂时没写，目的是用来做身份验证用的，暂时可写可不写", auth = true)
     JsonObject login(
             @Param("参数相关") String name,
             @Param("密码") Integer pwd,
-            @Param("是否为角色") Boolean isRole
+            @Param(value = "是否为角色,optional=true代表这个参数可以传可以不穿", optional = true) Boolean isRole
     );
 
 }
