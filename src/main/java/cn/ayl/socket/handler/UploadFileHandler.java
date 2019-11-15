fc964777d35d6614f36ac692c7dc7e86d1457052
anyongliang <AnYongLiang4869@163.com>
2019-11-15 20:36:48 +0800

    修改：稍微细化一下upload

--- src/main/java/cn/ayl/socket/handler/UploadFileHandler.java
+++ src/main/java/cn/ayl/socket/handler/UploadFileHandler.java
@@ -1,6 +1,7 @@
 package cn.ayl.socket.handler;
 
 import cn.ayl.util.json.JsonObject;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.*;
@@ -26,10 +27,9 @@ public class UploadFileHandler {
     private final HttpDataFactory factory = new DefaultHttpDataFactory(DefaultHttpDataFactory.MINSIZE);
     private HttpData partialContent;
     private HttpPostRequestDecoder decoder;
-    private Map<String, String> attrs = new ConcurrentHashMap<>();
-    protected boolean isMultipart = true;
-    private int fileSize = 177152;
-    private int fileBufferSize = 0;
+    private String filePath = "C:\\Users\\Rocy-Ayl\\Desktop\\";
+    private int fileSize;
+    private String fileExt;
     private String fileName;
     private FileChannel fileChannel;
     protected ByteBuffer fileBuffer;
@@ -63,31 +63,20 @@ public class UploadFileHandler {
         }
 
         HttpHeaders headers = request.headers();
-        isMultipart = decoder.isMultipart();
-
+        fileSize = headers.getInt("fileSize");
+        fileName = headers.get("fileName");
+        fileExt = headers.get("fileExt");
+        filePath = filePath + fileName + "." + fileExt;
+        InterfaceHttpData data = null;
         try {
-            fileChannel = new RandomAccessFile("/workspace/保密吗.doc", "rw").getChannel();
+            fileChannel = new RandomAccessFile(filePath, "rw").getChannel();
             fileBuffer = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, fileSize);
-            readHttpDataChunkByChunk();
-            response = ResponseHandler.responseOKAndJson(HttpResponseStatus.OK, JsonObject.Success());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        // 发送响应并关闭连接
-        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
-    }
-
-    private void writeHttpData( InterfaceHttpData data) throws IOException {
-        if (data.getHttpDataType() == InterfaceHttpData.HttpDataType.Attribute) {
-            Attribute attribute = (Attribute) data;
-            String value;
-            try {
-                value = attribute.getString(CharsetUtil.UTF_8);
-                attrs.put(attribute.getName(), value);
-            } catch (IOException e1) {
-                return;
+            data = decoder.next();
+            if (data != null) {
+                if (partialContent == data) {
+                    partialContent = null;
+                }
             }
-        } else {
             if (data.getHttpDataType() == InterfaceHttpData.HttpDataType.FileUpload) {
                 FileUpload fileUpload = (FileUpload) data;
                 if (fileUpload.isCompleted()) {
@@ -101,35 +90,15 @@ public class UploadFileHandler {
                     logger.info("upload FileName=[{}] success.", fileName);
                 }
             }
-        }
-    }
 
-    private void readHttpDataChunkByChunk() {
-        try {
-            while (decoder.hasNext()) {
-                InterfaceHttpData data = decoder.next();
-                if (data != null) {
-                    if (partialContent == data) {
-                        partialContent = null;
-                    }
-                    try {
-                        writeHttpData(data);
-                    } catch (Exception e) {
-                    } finally {
-                        data.release();
-                    }
-                }
-            }
-            InterfaceHttpData data = decoder.currentPartialHttpData();
-            if (data != null) {
-                if (partialContent == null) {
-                    partialContent = (HttpData) data;
-                }
-            }
-        } catch (HttpPostRequestDecoder.EndOfDataDecoderException e1) {
-            logger.error("readHttpDataChunkByChunk", e1);
+            response = ResponseHandler.responseOKAndJson(HttpResponseStatus.OK, JsonObject.Success());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            data.release();
         }
-
+        // 发送响应并关闭连接
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
     }
 
 }
