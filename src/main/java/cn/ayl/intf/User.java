40e2dead1ccf4beb79ead3bfe583c865170b00ca
Rock-Ayl <AnYongLiang4869@163.com>
2020-01-03 13:36:55 +0800

    修改：加一个用来测试的接口

---
+++ src/main/java/cn/ayl/intf/User.java
@@ -0,0 +1,12 @@
+package cn.ayl.intf;
+
+import cn.ayl.annotation.Service;
+
+/**
+ * created by Rock-Ayl 2020/1/3
+ * 用来测试
+ */
+@Service(desc = "测试相关,用来测试没有服务会怎样")
+public interface User extends IMicroService {
+
+}
