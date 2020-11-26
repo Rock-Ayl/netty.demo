5d1b7f43ce4b965737e349cc4da51b8b3519400c
Rock-Ayl <AnYongLiang4869@163.com>
2020-11-26 10:28:25 +0800

    修改：tika优化,解决了docx出现重复的问题

--- src/main/java/cn/ayl/util/TikaUtils.java
+++ src/main/java/cn/ayl/util/TikaUtils.java
@@ -1,10 +1,17 @@
 package cn.ayl.util;
 
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.apache.tika.Tika;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.tika.exception.TikaException;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * created by Rock-Ayl on 2020-11-5
@@ -12,23 +19,54 @@ import java.io.File;
  */
 public class TikaUtils {
 
-    protected static Logger logger = LoggerFactory.getLogger(TikaUtils.class);
+    //存储着需要特殊处理的文件真实类型
+    public static Map<String, String> TiKaFileRealTypeMaps = new HashMap<>();
+
+    //初始化需要做特殊处理的文件真实类型
+    static {
+        //docx的类型码,因为docx处理出现重复文本,故而特殊处理
+        TiKaFileRealTypeMaps.put("application/vnd.openxmlformats-officedocument.wordprocessingml.document", "docx");
+    }
 
     /**
      * 抽取文件文本内容
      *
      * @param file 文件对象
      * @return
+     * @throws IOException
+     * @throws TikaException
      */
-    public static String getContext(File file) {
-        try {
-            //解析并返回
-            new Tika().parseToString(file);
-        } catch (Exception e) {
-            logger.error("抽取文件[{}]失败,e:[{}]", file.getName(), e);
+    public static String getContext(File file) throws IOException, TikaException {
+        //创建一个tika
+        Tika tika = new Tika();
+        //文件真实类型
+        String fileRealExt = TiKaFileRealTypeMaps.get(tika.detect(file));
+        //判空
+        if (fileRealExt != null) {
+            //根据文件真实类型做处理
+            switch (fileRealExt) {
+                //docx由于重复、乱码问题,需要这么做
+                case "docx":
+                    //结果初始化
+                    StringBuffer str = new StringBuffer();
+                    //获取docx
+                    List<XWPFParagraph> paraGraph = new XWPFDocument(new FileInputStream(file)).getParagraphs();
+                    //循环
+                    for (XWPFParagraph para : paraGraph) {
+                        //循环
+                        for (XWPFRun r : para.getRuns()) {
+                            //当前文本位置
+                            int i = 0;
+                            //记录
+                            str.append(r.getText(i++));
+                        }
+                    }
+                    //返回docx全内容
+                    return str.toString();
+            }
         }
-        //缺省
-        return "";
+        //默认抽取并返回
+        return new Tika().parseToString(file);
     }
 
 }  
